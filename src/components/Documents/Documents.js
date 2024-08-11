d5aad04b085e797dba40c15056caa66d1a4cb356
Paul Rehbein <pre142604@stud.gibb.ch>
2024-08-11 14:57:06 +0200

    readded filterfuction

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -17,11 +17,11 @@ function Documents({ particlesEnabled }) {
   const [previewFile, setPreviewFile] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true); // Zustand für Ladeanzeige
 
   const supportedFileTypes = ['pdf', 'jpg', 'jpeg', 'png'];
 
-  //const API_BASE_URL = 'http://localhost:3000';   
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
   useEffect(() => {
@@ -51,19 +51,19 @@ function Documents({ particlesEnabled }) {
   }, [token]);
 
   const fetchFiles = async () => {
-    setLoading(true);
+    setLoading(true); // Ladeanzeige aktivieren
     try {
       const response = await axios.get(`${API_BASE_URL}/api/files`, {
         headers: { 'Authorization': `Bearer ${token}` }
       });
-      console.log('Files fetched:', response.data);
-      setFiles(response.data.files || []); // Sicherstellen, dass files immer ein Array ist
+      console.log('Files fetched:', response.data); // Überprüfen Sie hier die Antwort
+      setFiles(response.data.files);
       setError('');
     } catch (error) {
       console.error('Error fetching files:', error);
       setError(t('error_fetching_files'));
     } finally {
-      setLoading(false);
+      setLoading(false); // Ladeanzeige deaktivieren
     }
   };
 
@@ -176,6 +176,14 @@ function Documents({ particlesEnabled }) {
     renewToken();
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredFiles = files.length > 0 
+    ? files.filter(file => file.fileName.toLowerCase().includes(searchTerm.toLowerCase()))
+    : []; // Nur filtern, wenn Dateien vorhanden sind
+
   if (!token) {
     return <Login setToken={setToken} />;
   }
@@ -194,23 +202,94 @@ function Documents({ particlesEnabled }) {
             {t('download_files_title')}
             <strong className="main-name"> {t('files')}</strong>
           </h1>
+          <div id="poda">
+            <div className="glow"></div>
+            <div className="darkBorderBg"></div>
+            <div className="darkBorderBg"></div>
+            <div className="darkBorderBg"></div>
+            <div className="white"></div>
+            <div className="border"></div>
+            <div id="main">
+              <input
+                placeholder={t('search_placeholder')}
+                type="text"
+                name="text"
+                className="inputbar"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+              <div id="input-mask"></div>
+              <div id="pink-mask"></div>
+              <div className="filterBorder"></div>
+              <div id="filter-icon">
+                <svg
+                  preserveAspectRatio="none"
+                  height="27"
+                  width="27"
+                  viewBox="4.8 4.56 14.832 15.408"
+                  fill="none"
+                >
+                  <path
+                    d="M8.16 6.65002H15.83C16.47 6.65002 16.99 7.17002 16.99 7.81002V9.09002C16.99 9.56002 16.7 10.14 16.41 10.43L13.91 12.64C13.56 12.93 13.33 13.51 13.33 13.98V16.48C13.33 16.83 13.1 17.29 12.81 17.47L12 17.98C11.24 18.45 10.2 17.92 10.2 16.99V13.91C10.2 13.5 9.97 12.98 9.73 12.69L7.52 10.36C7.23 10.08 7 9.55002 7 9.20002V7.87002C7 7.17002 7.52 6.65002 8.16 6.65002Z"
+                    className="filtericoncolor"
+                    stroke-width="1"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <div id="search-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  height="24"
+                  fill="none"
+                  className="feather feather-search"
+                >
+                  <circle stroke="url(#search)" r="8" cy="11" cx="11"></circle>
+                  <line
+                    stroke="url(#searchl)"
+                    y2="16.65"
+                    y1="22"
+                    x2="16.65"
+                    x1="22"
+                  ></line>
+                  <defs>
+                    <linearGradient gradientTransform="rotate(50)" id="search">
+                      <stop stop-color="#f8e7f8" offset="0%"></stop>
+                      <stop stop-color="#b6a9b7" offset="50%"></stop>
+                    </linearGradient>
+                    <linearGradient id="searchl">
+                      <stop stop-color="#b6a9b7" offset="0%"></stop>
+                      <stop stop-color="#837484" offset="50%"></stop>
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+            </div>
+          </div>
 
           {loading ? (
             <div className="loading-container">
               <Spinner animation="border" role="status" color="#c770f0" className="sr-only">
-                <span ></span>
+                <span></span>
               </Spinner>
             </div>
           ) : (
             <>
-              {files.length === 0 && <Alert variant="info">{t('no_files_available')}</Alert>}
+              {filteredFiles.length === 0 && files.length > 0 && <Alert variant="info">{t('no_files_available')}</Alert>}
               {error && <Alert variant="danger">{error}</Alert>}
 
               <br />
 
               <Row>
                 <Col>
-                  {files.map((file, index) => (
+                  {filteredFiles.map((file, index) => (
                     <div key={index} className="file-item">
                       <input
                         className="file-checkbox"
@@ -254,7 +333,7 @@ function Documents({ particlesEnabled }) {
             <Modal.Title>{t('image_preview')}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div className="modal-content" >
+            <div className="modal-content">
               {previewFile && <img src={`${API_BASE_URL}/uploads/${previewFile}`} alt={t('image_preview')} style={{ width: '100%', borderRadius: '10px' }} />}
             </div>
           </Modal.Body>
