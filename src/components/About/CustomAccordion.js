4e7b917cfbc9a831ec13b9d394d80b5c4bc60818
Paul Rehbein <pre142604@stud.gibb.ch>
2024-08-21 14:40:12 +0200

    translations added

--- src/components/About/CustomAccordion.js
+++ src/components/About/CustomAccordion.js
@@ -1,36 +1,37 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import SkillItem from './SkillItem';
 import { Row, Col } from 'react-bootstrap';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import Typewriter from 'typewriter-effect';
+import { useTranslation } from 'react-i18next';
 
 function CustomAccordion({ sections }) {
+  const { t, i18n } = useTranslation();
   const [openIndices, setOpenIndices] = useState([]);
-  const [typewriterInstances, setTypewriterInstances] = useState({}); 
-  const [closingIndices, setClosingIndices] = useState(new Set()); 
+  const [typewriterInstances, setTypewriterInstances] = useState({});
+  const [closingIndices, setClosingIndices] = useState(new Set());
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
 
   const toggleSection = useCallback((index) => {
     if (openIndices.includes(index)) {
-      setClosingIndices(prev => new Set(prev.add(index)));
+      setClosingIndices((prev) => new Set(prev.add(index)));
       if (typewriterInstances[index]) {
         typewriterInstances[index]
-
-          .deleteAll(50)         
-
+          .deleteAll(50)
           .callFunction(() => {
             setTimeout(() => {
-              setOpenIndices(openIndices.filter(i => i !== index));
-              setClosingIndices(prev => {
+              setOpenIndices(openIndices.filter((i) => i !== index));
+              setClosingIndices((prev) => {
                 const newSet = new Set(prev);
                 newSet.delete(index);
                 return newSet;
               });
-            }, 500); 
-          })          
+            }, 0);
+          })
           .start();
       } else {
-        setOpenIndices(openIndices.filter(i => i !== index));
-        setClosingIndices(prev => {
+        setOpenIndices(openIndices.filter((i) => i !== index));
+        setClosingIndices((prev) => {
           const newSet = new Set(prev);
           newSet.delete(index);
           return newSet;
@@ -41,16 +42,31 @@ function CustomAccordion({ sections }) {
     }
   }, [openIndices, typewriterInstances]);
 
+  // Watch for language changes and reset Typewriter
+  useEffect(() => {
+    if (currentLanguage !== i18n.language) {
+      setCurrentLanguage(i18n.language);
+
+      Object.keys(typewriterInstances).forEach((index) => {
+        const instance = typewriterInstances[index];
+        if (instance) {
+          typewriterInstances[index]
+          .deleteAll(50)
+          .start();
+        }
+      });
+    }
+  }, [i18n.language, currentLanguage, typewriterInstances]);
+
   const calculateAverage = (skills) => {
     const total = skills.reduce((sum, skill) => sum + skill.level, 0);
     return (total / skills.length).toFixed(2);
   };
 
   return (
-    <div className="custom-accordion" style={{ paddingBottom: "50px" }}>
+    <div className="custom-accordion" style={{ paddingBottom: '50px' }}>
       {sections.map((section, index) => {
         const isOpen = openIndices.includes(index);
-        const isClosing = closingIndices.has(index);
         const average = isOpen ? calculateAverage(section.skills) : '';
 
         return (
@@ -64,21 +80,20 @@ function CustomAccordion({ sections }) {
                 <FaChevronUp />
               </span>
             </div>
-            <div
-              className={`accordion-body ${isOpen ? 'open' : ''}`}
-            >
-              <div className={`average-text ${isOpen ? 'open' : ''}`} >
+            <div className={`accordion-body ${isOpen ? 'open' : ''}`}>
+              <div className="average-text" style={{ marginTop: '10px', textAlign: 'center' }}>
               {isOpen && (
-                <div className={`average-text ${isOpen ? 'open' : ''}`} >
+                
                   <Typewriter
+                    key={i18n.language} // Re-render Typewriter on language change
                     onInit={(typewriter) => {
-                      setTypewriterInstances(prev => ({
+                      setTypewriterInstances((prev) => ({
                         ...prev,
-                        [index]: typewriter
+                        [index]: typewriter,
                       }));
                       typewriter
-                        .typeString(`Average: ${average}%`)
-                        .start()
+                        .typeString(`${t('average')}: ${average}%`)
+                        .start();
                     }}
                     options={{
                       delay: 50,
@@ -86,9 +101,9 @@ function CustomAccordion({ sections }) {
                       loop: false,
                     }}
                   />
-                </div>
+                
               )}</div>
-              <Row style={{ justifyContent: "center" }}>
+              <Row style={{ justifyContent: 'center' }}>
                 {section.skills.map((skill, skillIndex) => (
                   <Col xs={12} md={6} key={skillIndex} className="tech-col">
                     <SkillItem skill={skill} />
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,71 +2,74 @@ import React from 'react';
 import CustomAccordion from './CustomAccordion'; 
 import { SiNextdotjs, SiJavascript, SiReact, SiGit, SiMysql, SiMongodb, SiPython, SiRedis, SiSqlite, SiHtml5, SiCss3, SiMicrosoftazure, SiCsharp, SiDocker, SiKubernetes, SiPortainer, SiAzuredevops, SiAmazonaws, SiGitlab, SiGithub, SiFirebase, SiExpress, SiPostgresql, SiPhp, SiFlask, SiDotnet } from 'react-icons/si';
 import { FaNodeJs, FaJava } from "react-icons/fa";
-
-const skillCategories = [
-  {
-    category: "Web Development",
-    skills: [
-      { icon: <SiJavascript />, name: "JavaScript", level: 90 },
-      { icon: <SiReact />, name: "React", level: 90 },
-      { icon: <SiNextdotjs />, name: "Next.js", level: 80 },
-      { icon: <SiHtml5 />, name: "HTML", level: 95 },
-      { icon: <SiCss3 />, name: "CSS", level: 90 },
-      { icon: <FaNodeJs />, name: "Node.js", level: 85 },
-      { icon: <SiExpress />, name: "Express.js", level: 80 },
-      { icon: <SiFlask />, name: "Flask", level: 80 }, // Flask hinzugefügt
-    ],
-  },
-  {
-    category: "App Development",
-    skills: [
-      { icon: <SiReact />, name: "React Native", level: 90 },
-      { icon: <SiDotnet />, name: ".NET MAUI", level: 70 }, // .NET MAUI in App Development verschoben
-    ],
-  },
-  {
-    category: "Programming Languages",
-    skills: [
-      { icon: <SiPython />, name: "Python", level: 90 },
-      { icon: <FaJava />, name: "Java", level: 80 },
-      { icon: <SiCsharp />, name: "C#", level: 80 },
-      { icon: <SiPhp />, name: "PHP", level: 80 },
-      { icon: <SiDotnet />, name: ".NET", level: 75 }, // .NET als Programmiersprache
-    ],
-  },
-  {
-    category: "Databases",
-    skills: [
-      { icon: <SiMongodb />, name: "MongoDB", level: 75 },
-      { icon: <SiMysql />, name: "MySQL", level: 85 },
-      { icon: <SiSqlite />, name: "SQLite", level: 95 },
-      { icon: <SiPostgresql />, name: "PostgreSQL", level: 95 },
-      { icon: <SiRedis />, name: "Redis", level: 65 },
-    ],
-  },
-  {
-    category: "Cloud & DevOps",
-    skills: [
-      { icon: <SiAmazonaws />, name: "AWS", level: 75 },
-      { icon: <SiMicrosoftazure />, name: "Azure", level: 80 },
-      { icon: <SiFirebase />, name: "Firebase", level: 70 },
-      { icon: <SiDocker />, name: "Docker", level: 85 },
-      { icon: <SiKubernetes />, name: "Kubernetes", level: 70 },
-      { icon: <SiPortainer />, name: "Portainer", level: 65 },
-      { icon: <SiAzuredevops />, name: "Azure DevOps", level: 80 },
-    ],
-  },
-  {
-    category: "Tools & Extensions",
-    skills: [
-      { icon: <SiGit />, name: "Git", level: 95 },
-      { icon: <SiGithub />, name: "GitHub", level: 90 },
-      { icon: <SiGitlab />, name: "GitLab", level: 85 },
-    ],
-  },
-];
+import { useTranslation } from 'react-i18next';
 
 function Techstack() {
+  const { t } = useTranslation();
+
+  const skillCategories = [
+    {
+      category: t("skills.web_development"), // Web Development -> Webentwicklung
+      skills: [
+        { icon: <SiJavascript />, name: "JavaScript", level: 90 },
+        { icon: <SiReact />, name: "React", level: 90 },
+        { icon: <SiNextdotjs />, name: "Next.js", level: 80 },
+        { icon: <SiHtml5 />, name: "HTML", level: 95 },
+        { icon: <SiCss3 />, name: "CSS", level: 90 },
+        { icon: <FaNodeJs />, name: "Node.js", level: 85 },
+        { icon: <SiExpress />, name: "Express.js", level: 80 },
+        { icon: <SiFlask />, name: "Flask", level: 80 },
+      ],
+    },
+    {
+      category: t("skills.app_development"), // App Development -> App-Entwicklung
+      skills: [
+        { icon: <SiReact />, name: "React Native", level: 90 },
+        { icon: <SiDotnet />, name: ".NET MAUI", level: 70 },
+      ],
+    },
+    {
+      category: t("skills.programming_languages"), // Programming Languages -> Programmiersprachen
+      skills: [
+        { icon: <SiPython />, name: "Python", level: 90 },
+        { icon: <FaJava />, name: "Java", level: 80 },
+        { icon: <SiCsharp />, name: "C#", level: 80 },
+        { icon: <SiPhp />, name: "PHP", level: 80 },
+        { icon: <SiDotnet />, name: ".NET", level: 75 },
+      ],
+    },
+    {
+      category: t("skills.databases"), // Databases -> Datenbanken
+      skills: [
+        { icon: <SiMongodb />, name: "MongoDB", level: 75 },
+        { icon: <SiMysql />, name: "MySQL", level: 85 },
+        { icon: <SiSqlite />, name: "SQLite", level: 95 },
+        { icon: <SiPostgresql />, name: "PostgreSQL", level: 95 },
+        { icon: <SiRedis />, name: "Redis", level: 65 },
+      ],
+    },
+    {
+      category: t("skills.cloud_devops"), // Cloud & DevOps -> Cloud & DevOps
+      skills: [
+        { icon: <SiAmazonaws />, name: "AWS", level: 75 },
+        { icon: <SiMicrosoftazure />, name: "Azure", level: 80 },
+        { icon: <SiFirebase />, name: "Firebase", level: 70 },
+        { icon: <SiDocker />, name: "Docker", level: 85 },
+        { icon: <SiKubernetes />, name: "Kubernetes", level: 70 },
+        { icon: <SiPortainer />, name: "Portainer", level: 65 },
+        { icon: <SiAzuredevops />, name: "Azure DevOps", level: 80 },
+      ],
+    },
+    {
+      category: t("skills.tools_extensions"), // Tools & Extensions -> Werkzeuge & Erweiterungen
+      skills: [
+        { icon: <SiGit />, name: "Git", level: 95 },
+        { icon: <SiGithub />, name: "GitHub", level: 90 },
+        { icon: <SiGitlab />, name: "GitLab", level: 85 },
+      ],
+    },
+  ];
+
   return (
     <div>
       <CustomAccordion sections={skillCategories} />
