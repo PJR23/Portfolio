f820534a4cc0cd3bece3951396354c9f68fa706e
Paul Rehbein <pre142604@stud.gibb.ch>
2024-08-21 19:15:21 +0200

    filter projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,28 +3,40 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import { FiDownload } from "react-icons/fi";
+import { FiDownload, FiEye } from "react-icons/fi";
+import { useTranslation } from 'react-i18next';
 
 function ProjectCards(props) {
+  const { t } = useTranslation();
+  
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ maxHeight: "260px", width: "auto" }} />
+    <Card className="project-card-view">        
       <Card.Body>
+        {/* Programmiersprachen-Tags */}
+        {props.languages && (
+              <div className="language-tags">
+                {props.languages.map((lang, index) => (
+                  <span key={index} className="language-tag">{lang}</span>
+                ))}
+              </div>
+            )}
+        
+        <div className="cardimg">
+          <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ maxHeight: "260px", width: "auto" }} />
+        </div>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text className={"cardtxt"}>
           {props.description}
         </Card.Text>
-
+        
         {/* GitHub-Button nur rendern, wenn ghLink vorhanden ist */}
         {props.ghLink && (
           <Button variant="primary" href={props.ghLink} target="_blank" style={{ margin: "5px" }}>
             <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
+            {props.isBlog ? t('Blog') : t('GitHub')}
           </Button>
         )}
-        {"\n"}
-        {"\n"}
-
+        
         {/* Demo-Link-Button nur rendern, wenn demoLink vorhanden ist */}
         {!props.isBlog && props.demoLink && (
           <Button
@@ -34,10 +46,23 @@ function ProjectCards(props) {
             style={{ margin: "5px" }}
           >
             <CgWebsite /> &nbsp;
-            {"Demo"}
+            {t('Demo')}
           </Button>
         )}
-
+        
+        {/* Vorschau-Button nur rendern, wenn previewLink vorhanden ist */}
+        {props.previewLink && (
+          <Button
+            variant="primary"
+            href={props.previewLink}
+            target="_blank"
+            style={{ margin: "5px" }}
+          >
+            <FiEye /> &nbsp;
+            {t('Preview Abstract')}
+          </Button>
+        )}
+        
         {/* Download-Button nur rendern, wenn abstract vorhanden ist */}
         {props.abstract && (
           <Button
@@ -48,7 +73,7 @@ function ProjectCards(props) {
             download
           >
             <FiDownload /> &nbsp;
-            {"Abstract"}
+            {t('Download Abstract')}
           </Button>
         )}
       </Card.Body>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import { useTranslation } from 'react-i18next';
@@ -11,6 +11,7 @@ import wormgame from "../../Assets/Projects/worm.png";
 import facereg from "../../Assets/Projects/facereg.png";
 import m293website from "../../Assets/Projects/m293webseite.png";
 import studypal from "../../Assets/Projects/studypal.png";
+import { FaSearch } from "react-icons/fa";
 
 // Abstract-Dateipfade
 import abstractStudypalEN from "../../Assets/Projects/abstracts/en/studypal.pdf";
@@ -31,14 +32,98 @@ import abstractFirstwebsiteDE from "../../Assets/Projects/abstracts/de/firstwebs
 function Projects({ particlesEnabled }) {
   const { t, i18n } = useTranslation();
   const currentLanguage = i18n.language;
+  
+  const [searchTerm, setSearchTerm] = useState("");
 
   // Funktion, um den korrekten Abstract-Pfad basierend auf der aktuellen Sprache zu wählen
   const getAbstractPath = (abstractEN, abstractDE) => {
     return currentLanguage === "de" ? abstractDE : abstractEN;
   };
 
+  // Filter-Funktion für die Projekte basierend auf dem Suchbegriff
+  const projects = [
+    {
+      imgPath: studypal,
+      title: t('studypal_title'),
+      description: t('studypal_description'),
+      ghLink: "https://github.com/PJR23/StudyPal",
+      languages: ["React Native", "JavaScript", "SQLite"],
+      previewLink: getAbstractPath(abstractStudypalEN, abstractStudypalDE),
+      abstract: getAbstractPath(abstractStudypalEN, abstractStudypalDE),
+    },
+    {
+      imgPath: facereg,
+      title: t('facelogin_title'),
+      description: t('facelogin_description'),
+      ghLink: "https://github.com/PJR23/Facelogin-Calendar-App",
+      languages: ["Python", "Flask", "HTML", "CSS", "JavaScript", "SQLite"],
+      previewLink: getAbstractPath(abstractFaceregEN, abstractFaceregDE),
+      abstract: getAbstractPath(abstractFaceregEN, abstractFaceregDE),
+    },
+    {
+      imgPath: onlineshop,
+      title: t('online_shop_title'),
+      description: t('online_shop_description'),
+      ghLink: "https://github.com/PJR23/Online-Shop",
+      languages: ["PHP", "HTML", "CSS", "JavaScript", "MongoDB", "Apache2"],
+      previewLink: getAbstractPath(abstractOnlineshopEN, abstractOnlineshopDE),
+      abstract: getAbstractPath(abstractOnlineshopEN, abstractOnlineshopDE),
+    },
+    {
+      imgPath: taskmaster,
+      title: t('taskmaster_title'),
+      description: t('taskmaster_description'),
+      ghLink: "https://github.com/PJR23/TaskMaster",
+      languages: ["C#", "XAML", ".NET MAUI", "SQLite"],
+      previewLink: getAbstractPath(abstractTaskmasterEN, abstractTaskmasterDE),
+      abstract: getAbstractPath(abstractTaskmasterEN, abstractTaskmasterDE),
+    },
+    {
+      imgPath: m293website,
+      title: t('m293_website_title'),
+      description: t('m293_website_description'),
+      ghLink: "https://github.com/PJR23/M293-Website",
+      languages: ["HTML", "CSS", "JavaScript"],
+      previewLink: getAbstractPath(abstractM293websiteEN, abstractM293websiteDE),
+      abstract: getAbstractPath(abstractM293websiteEN, abstractM293websiteDE),
+    },
+    {
+      imgPath: wormgame,
+      title: t('worm_title'),
+      description: t('worm_description'),
+      ghLink: "https://github.com/soumyajit4419/Plant_AI",
+      languages: ["HTML", "CSS", "JavaScript"],
+      previewLink: getAbstractPath(abstractWormgameEN, abstractWormgameDE),
+      abstract: getAbstractPath(abstractWormgameEN, abstractWormgameDE),
+    },
+    {
+      imgPath: websitev2,
+      title: t('first_website_v2_title'),
+      description: t('first_website_v2_description'),
+      ghLink: "https://github.com/PJR23/First-Website-v2",
+      languages: ["HTML", "CSS", "JavaScript"],
+      previewLink: getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE),
+      abstract: getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE),
+    },
+    {
+      imgPath: firstwebsite,
+      title: t('first_website_title'),
+      description: t('first_website_description'),
+      ghLink: "https://github.com/PJR23/First-Website",
+      languages: ["HTML", "CSS"],
+      previewLink: getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE),
+      abstract: getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE),
+    },
+  ]
+  
+  const filteredProjects = projects.filter(project =>
+    project.languages.some(language =>
+      language.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
+
   return (
-    <Container fluid className="project-section">
+    <Container fluid className="project-section" >
       {particlesEnabled && <Particle />}
       <Container>
         <h1 className="project-heading">
@@ -47,101 +132,93 @@ function Projects({ particlesEnabled }) {
         <p>
           {t('project_description')}
         </p>
+
+        <div id="poda">
+            <div className="glow"></div>
+            <div className="darkBorderBg"></div>
+            <div className="darkBorderBg"></div>
+            <div className="darkBorderBg"></div>
+            <div className="white"></div>
+            <div className="border"></div>
+            <div id="main">
+              <input
+                placeholder={t('search_placeholder')}
+                type="text"
+                name="text"
+                className="inputbar"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <div id="input-mask"></div>
+              <div id="pink-mask"></div>
+              <div className="filterBorder"></div>
+              <div id="filter-icon">
+                <svg
+                  preserveAspectRatio="none"
+                  height="27"
+                  width="27"
+                  viewBox="4.8 4.56 14.832 15.408"
+                  fill="none"
+                >
+                  <path
+                    d="M8.16 6.65002H15.83C16.47 6.65002 16.99 7.17002 16.99 7.81002V9.09002C16.99 9.56002 16.7 10.14 16.41 10.43L13.91 12.64C13.56 12.93 13.33 13.51 13.33 13.98V16.48C13.33 16.83 13.1 17.29 12.81 17.47L12 17.98C11.24 18.45 10.2 17.92 10.2 16.99V13.91C10.2 13.5 9.97 12.98 9.73 12.69L7.52 10.36C7.23 10.08 7 9.55002 7 9.20002V7.87002C7 7.17002 7.52 6.65002 8.16 6.65002Z"
+                    className="filtericoncolor"
+                    stroke-width="1"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </div>
+              <div id="search-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  height="24"
+                  fill="none"
+                  className="feather feather-search"
+                >
+                  <circle stroke="url(#search)" r="8" cy="11" cx="11"></circle>
+                  <line
+                    stroke="url(#searchl)"
+                    y2="16.65"
+                    y1="22"
+                    x2="16.65"
+                    x1="22"
+                  ></line>
+                  <defs>
+                    <linearGradient gradientTransform="rotate(50)" id="search">
+                      <stop stop-color="#f8e7f8" offset="0%"></stop>
+                      <stop stop-color="#b6a9b7" offset="50%"></stop>
+                    </linearGradient>
+                    <linearGradient id="searchl">
+                      <stop stop-color="#b6a9b7" offset="0%"></stop>
+                      <stop stop-color="#837484" offset="50%"></stop>
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+            </div>
+          </div>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={studypal}
-              isBlog={false}
-              title={t('studypal_title')}
-              description={t('studypal_description')}
-              ghLink="https://github.com/PJR23/StudyPal"
-              // demoLink=""
-              abstract={getAbstractPath(abstractStudypalEN, abstractStudypalDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={facereg}
-              isBlog={false}
-              title={t('facelogin_title')}
-              description={t('facelogin_description')}
-              ghLink="https://github.com/PJR23/Facelogin-Calendar-App"
-              // demoLink=""
-              abstract={getAbstractPath(abstractFaceregEN, abstractFaceregDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={onlineshop}
-              isBlog={false}
-              title={t('online_shop_title')}
-              description={t('online_shop_description')}
-              ghLink="https://github.com/PJR23/Online-Shop"
-              // demoLink=""
-              abstract={getAbstractPath(abstractOnlineshopEN, abstractOnlineshopDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={taskmaster}
-              isBlog={false}
-              title={t('taskmaster_title')}
-              description={t('taskmaster_description')}
-              ghLink="https://github.com/PJR23/TaskMaster"
-              abstract={getAbstractPath(abstractTaskmasterEN, abstractTaskmasterDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={m293website}
-              isBlog={false}
-              title={t('m293_website_title')}
-              description={t('m293_website_description')}
-              ghLink="https://github.com/PJR23/M293-Website"
-              // demoLink=""
-              abstract={getAbstractPath(abstractM293websiteEN, abstractM293websiteDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={wormgame}
-              isBlog={false}
-              title={t('worm_title')}
-              description={t('worm_description')}
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              // demoLink=""
-              abstract={getAbstractPath(abstractWormgameEN, abstractWormgameDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={websitev2}
-              isBlog={false}
-              title={t('first_website_v2_title')}
-              description={t('first_website_v2_description')}
-              ghLink="https://github.com/PJR23/First-Website-v2"
-              // demoLink=""
-              abstract={getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE)}
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={firstwebsite}
-              isBlog={false}
-              title={t('first_website_title')}
-              description={t('first_website_description')}
-              ghLink="https://github.com/PJR23/First-Website"      
-              // demoLink=""
-              abstract={getAbstractPath(abstractFirstwebsiteEN, abstractFirstwebsiteDE)}
-            />
-          </Col>
+          {filteredProjects.map((project, index) => (
+            <Col key={index} md={4} className="project-card">
+              <ProjectCard
+                imgPath={project.imgPath}
+                isBlog={false}
+                title={project.title}
+                description={project.description}
+                ghLink={project.ghLink}
+                languages={project.languages}
+                previewLink={project.previewLink}
+                abstract={project.abstract}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
