84aac29322b803f8b2ddae1441f81483d1148116
Paul Rehbein <pre142604@stud.gibb.ch>
2024-08-23 23:19:59 +0200

    impressum added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import {
   AiFillGithub,
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
+import { IoDocumentText } from "react-icons/io5";
 import { FaLinkedinIn } from "react-icons/fa";
 import { useTranslation } from 'react-i18next';
 
@@ -12,6 +13,12 @@ function Footer() {
   const { t } = useTranslation();
   let date = new Date();
   let year = date.getFullYear();
+
+  // State to handle the Modal
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   return (
     <Container fluid className="footer">
@@ -64,9 +71,41 @@ function Footer() {
                 <AiFillInstagram />
               </a>
             </li>
+            <li className="social-icons">
+                <Button onClick={handleShow} className="socialicons impressumbutton" >
+                <IoDocumentText/>  Impressum
+              </Button>
+            </li>
           </ul>
         </Col>
       </Row>
+
+      <Modal show={showModal} onHide={handleClose} >
+        <Modal.Header >
+          <Modal.Title>{t('footer.impressum_title')}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="modal-content">
+          <p>{t('footer.impressum_content_line1')}</p>
+          <p>{t('footer.impressum_content_line2')}</p>
+          <p>{t('footer.impressum_content_line3')}</p>
+          <p>{t('footer.impressum_content_line4')}</p>
+          <p>{t('footer.impressum_content_line5')}</p>
+          <h5>{t('footer.impressum_disclaimer_title')}</h5>
+          <ul>
+            <li>{t('footer.impressum_disclaimer_item1')}</li>
+            <li>{t('footer.impressum_disclaimer_item2')}</li>
+            <li>{t('footer.impressum_disclaimer_item3')}</li>
+          </ul>
+          <p>{t('footer.impressum_disclaimer_warning')}</p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            {t('close')}
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
